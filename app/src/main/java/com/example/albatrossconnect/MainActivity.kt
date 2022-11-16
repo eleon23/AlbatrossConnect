196298621a0cc0776ae38b2018f6b2b40530546a
S SAI SURYATEJA <sakurusurya2000@gmail.com>
2022-11-16 13:29:31 -0600

    Basic layout

--- app/src/main/java/com/example/albatrossconnect/MainActivity.kt
+++ app/src/main/java/com/example/albatrossconnect/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.albatrossconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.Fragment
+import com.example.albatrossconnect.fragments.CourseFragment
+import com.example.albatrossconnect.fragments.ProfessorFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val courseFragment = CourseFragment()
+        val professorFragment = ProfessorFragment()
+
+
+
+        }
+
     }
+
 }
