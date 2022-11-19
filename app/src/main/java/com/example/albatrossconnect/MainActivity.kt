bba9515bfe93332678b76dec72a2a2cb1a3f37e2
eleon23 <eleon23@uic.edu>
2022-11-19 17:42:41 -0600

    Added navigation , view binding, did Course details, and Prerequisites screens

--- app/src/main/java/com/example/albatrossconnect/MainActivity.kt
+++ app/src/main/java/com/example/albatrossconnect/MainActivity.kt
@@ -2,19 +2,19 @@ package com.example.albatrossconnect
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.albatrossconnect.databinding.ActivityMainBinding
 import com.example.albatrossconnect.fragments.CourseFlow.CourseFragment
+import com.example.albatrossconnect.fragments.HomeFragment
 import com.example.albatrossconnect.fragments.ProfessorFlow.ProfessorFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val courseFragment = CourseFragment()
-        val professorFragment = ProfessorFragment()
-
-
-
-        }
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
 }
---
+++ app/src/main/java/com/example/albatrossconnect/fragments/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.albatrossconnect.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.albatrossconnect.R
+import com.example.albatrossconnect.databinding.ActivityMainBinding
+import com.example.albatrossconnect.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setUpOnClickListeners()
+    }
+
+    private fun setUpOnClickListeners(){
+        binding.courseDescriptions.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_homeFragment_to_courseFragment)
+        }
+    }
+}
