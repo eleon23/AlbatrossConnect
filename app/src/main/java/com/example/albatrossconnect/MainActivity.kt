10c004a6c7bc82ecc07a9f86349b5829215914b4
eleon23 <eleon23@uic.edu>
2022-11-23 17:22:52 -0600

    Test with graphql

--- app/src/main/java/com/example/albatrossconnect/MainActivity.kt
+++ app/src/main/java/com/example/albatrossconnect/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.albatrossconnect
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.http.HttpHeader
 import com.example.albatrossconnect.databinding.ActivityMainBinding
 import com.example.albatrossconnect.fragments.CourseFlow.CourseFragment
 import com.example.albatrossconnect.fragments.HomeFragment
 import com.example.albatrossconnect.fragments.ProfessorFlow.ProfessorFragment
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +21,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        GlobalScope.launch{
+            getSchools()
+        }
+
+    }
+
+    suspend fun getSchools() {
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("https://www.ratemyprofessors.com/graphql")
+            .httpHeaders(listOf(HttpHeader("authorization", "Basic dGVzdDp0ZXN0")))
+            .build()
+
+
+        val response = apolloClient.query(ProfessorQuery(query = "Chicago")).execute()
+
+        println("Hero.name=${response.operation}")
     }
 }
