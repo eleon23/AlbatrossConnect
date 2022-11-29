e33a8692472c8a1f431dd66e37c44a18e90b5887
S SAI SURYATEJA <sakurusurya2000@gmail.com>
2022-11-29 13:48:07 -0600

    RANDOM

--- app/src/main/java/com/example/albatrossconnect/data/Constants.java
+++ app/src/main/java/com/example/albatrossconnect/data/Constants.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Constants {
     /* Defining all the constants*/
     public static final List<String> movieNames = Arrays.asList(
-            "Offline", "Offline", "Online", "Offline", "Online", "Offline", "Online",
-            "Online", "Online", "Online", "Offline", "Offline", "Online", "Online");
+            "CS 594", "CS 478", "CS 478", "CS 522", "CS 529", "CS 506", "CS 428",
+            "CS 418", "CS 478", "CS 594", "CS 412", "CS 415", "CS 529", "CS 418");
     public static final List<String> dirNames = Arrays.asList(
             "Random1", "Random2", "Random3", "Random4", "Random5", "Random6", "Random7",
             "Random8", "Random9", "Random10", "Random11", "Random12", "Random13", "Random14");
